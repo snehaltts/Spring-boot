9014e4d88a9083bdb766b4f0fd04d80a757c2a55
snehaltts <78852647+snehaltts@users.noreply.github.com>
2021-03-17 15:36:05 +0530

    Add files via upload

---
+++ DemoSpringCons/src/main/java/com/ltts/DemoSpringCons/App.java
@@ -0,0 +1,18 @@
+package com.ltts.DemoSpringCons;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	  ApplicationContext context= new ClassPathXmlApplicationContext("bean.xml");
+    	  Employee e=(Employee) context.getBean("emp");
+    	  Employee e1=(Employee) context.getBean("emp1");
+    	  System.out.println(e);
+    	  System.out.println(e1);
+    }
+}
---
+++ DemoSpringCons/src/main/java/com/ltts/DemoSpringCons/Employee.java
@@ -0,0 +1,50 @@
+package com.ltts.DemoSpringCons;
+
+public class Employee {
+  
+	private int empid;
+	private String empname;
+	private String empaddress;
+	
+	public Employee(int empid, String empname, String empaddress) {
+		super();
+		this.empid = empid;
+		this.empname = empname;
+		this.empaddress = empaddress;
+	}
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getEmpaddress() {
+		return empaddress;
+	}
+	public void setEmpaddress(String empaddress) {
+		this.empaddress = empaddress;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", empname=" + empname + ", empaddress=" + empaddress + "]";
+	}
+	
+	
+	
+	
+}
